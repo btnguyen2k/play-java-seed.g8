d293d910003112c7dd87110b070fcf702002126e
Thanh Nguyen <btnguyen2k@gmail.com>
2018-01-08 21:59:10 +0700

    Pre template-v2.6.r4: create & list users

--- src/main/g8/app/samples/controllers/SampleControlPanelController.java
+++ src/main/g8/app/samples/controllers/SampleControlPanelController.java
@@ -10,11 +10,15 @@ import play.data.Form;
 import play.mvc.Result;
 import play.twirl.api.Html;
 import samples.bo.user.IUserDao;
+import samples.bo.user.UserBo;
 import samples.bo.user.UserGroupBo;
 import samples.compositions.AuthRequired;
+import samples.forms.FormCreateEditUser;
 import samples.forms.FormCreateEditUsergroup;
 import samples.models.UserGroupModel;
+import samples.models.UserModel;
 import samples.utils.SampleConstants;
+import samples.utils.UserUtils;
 
 /**
  * Sample control panel controller.
@@ -34,6 +38,8 @@ public class SampleControlPanelController extends BasePageController {
     public Result home() throws Exception {
         return ok(render(VIEW_HOME));
     }
+
+    /*----------------------------------------------------------------------*/
 
     public final static String VIEW_USERGROUPS = "vsamples.usergroups";
 
@@ -192,4 +198,172 @@ public class SampleControlPanelController extends BasePageController {
                             + calcMessages().at("msg.delete_usergroup.failed", bo.getId()));
         }
     }
+
+    /*----------------------------------------------------------------------*/
+
+    public final static String VIEW_USERS = "vsamples.users";
+
+    /**
+     * Handle GET /<context>/cp/users
+     */
+    public Result users() throws Exception {
+        IUserDao dao = getRegistry().getBean(IUserDao.class);
+        UserModel[] allUsers = UserModel.newInstances(dao.getAllUsers());
+
+        // Note: cast to Object to make sure the whole array is treated as a
+        // single parameter value!
+        return ok(render(VIEW_USERS, (Object) allUsers));
+    }
+
+    public final static String VIEW_CREATE_USER = "vsamples.create_user";
+
+    /**
+     * Handle GET /<context>/cp/createUser
+     */
+    public Result createUser() throws Exception {
+        Form<FormCreateEditUser> form = formFactory.form(FormCreateEditUser.class);
+        Html html = render(VIEW_CREATE_USER, form);
+        return ok(html);
+    }
+
+    /**
+     * Handle POST /<context>/cp/createUser
+     */
+    public Result createUserSubmit() throws Exception {
+        Form<FormCreateEditUser> form = formFactory.form(FormCreateEditUser.class)
+                .bindFromRequest(request());
+        if (form.hasErrors()) {
+            Html html = render(VIEW_CREATE_USER, form);
+            return ok(html);
+        }
+        FormCreateEditUser formData = form.get();
+        UserBo bo = UserBo.newInstance(formData.getUsername()).setEmail(formData.getEmail())
+                .setFullname(formData.getFullname()).setGroupId(formData.getGroupId());
+        bo.setPassword(UserUtils.encryptPassword(formData.getPassword()));
+        IUserDao dao = getRegistry().getBean(IUserDao.class);
+        DaoResult result = dao.create(bo);
+        if (result.getStatus() == DaoOperationStatus.SUCCESSFUL) {
+            return responseRedirect(samples.controllers.routes.SampleControlPanelController.users(),
+                    VIEW_USERS, calcMessages().at("msg.create_user.done", bo.getUsername()));
+        } else {
+            return responseRedirect(samples.controllers.routes.SampleControlPanelController.users(),
+                    VIEW_USERS, SampleConstants.FLASH_MSG_PREFIX_WARNING
+                            + calcMessages().at("msg.create_user.failed", bo.getUsername()));
+        }
+    }
+
+    // public final static String VIEW_EDIT_USERGROUP =
+    // "vsamples.edit_usergroup";
+    //
+    // /**
+    // * Handle GET /<context>/cp/editUsergroup?id=<usergroup-id>
+    // */
+    // public Result editUsergroup(String id) throws Exception {
+    // IUserDao dao = getRegistry().getBean(IUserDao.class);
+    // UserGroupBo bo = dao.getUserGroup(id);
+    // if (bo == null) {
+    // return responseRedirect(
+    // samples.controllers.routes.SampleControlPanelController.usergroups(),
+    // VIEW_USERGROUPS, SampleConstants.FLASH_MSG_PREFIX_ERROR
+    // + calcMessages().at("error.usergroup.not_found", id));
+    // }
+    // Form<FormCreateEditUsergroup> form =
+    // formFactory.form(FormCreateEditUsergroup.class)
+    // .fill(FormCreateEditUsergroup.newInstance(bo));
+    // Html html = render(VIEW_EDIT_USERGROUP, form);
+    // return ok(html);
+    // }
+    //
+    // /**
+    // * Handle POST /<context>/cp/editUsergroup?id=<usergroup-id>
+    // */
+    // public Result editUsergroupSubmit(String id) throws Exception {
+    // IUserDao dao = getRegistry().getBean(IUserDao.class);
+    // UserGroupBo bo = dao.getUserGroup(id);
+    // if (bo == null) {
+    // return responseRedirect(
+    // samples.controllers.routes.SampleControlPanelController.usergroups(),
+    // VIEW_USERGROUPS, SampleConstants.FLASH_MSG_PREFIX_ERROR
+    // + calcMessages().at("error.usergroup.not_found", id));
+    // }
+    // Form<FormCreateEditUsergroup> form =
+    // formFactory.form(FormCreateEditUsergroup.class)
+    // .bindFromRequest(request());
+    // if (form.hasErrors()) {
+    // Html html = render(VIEW_EDIT_USERGROUP, form);
+    // return ok(html);
+    // }
+    // FormCreateEditUsergroup formData = form.get();
+    // bo.setDescription(formData.getDescription());
+    // DaoResult result = dao.update(bo);
+    // if (result.getStatus() == DaoOperationStatus.SUCCESSFUL) {
+    // return responseRedirect(
+    // samples.controllers.routes.SampleControlPanelController.usergroups(),
+    // VIEW_USERGROUPS, calcMessages().at("msg.edit_usergroup.done",
+    // bo.getId()));
+    // } else {
+    // return responseRedirect(
+    // samples.controllers.routes.SampleControlPanelController.usergroups(),
+    // VIEW_USERGROUPS, SampleConstants.FLASH_MSG_PREFIX_WARNING
+    // + calcMessages().at("msg.edit_usergroup.failed", bo.getId()));
+    // }
+    // }
+    //
+    // public final static String VIEW_DELETE_USERGROUP =
+    // "vsamples.delete_usergroup";
+    //
+    // /**
+    // * Handle GET /<context>/cp/deleteUsergroup?id=<usergroup-id>
+    // */
+    // public Result deleteUsergroup(String id) throws Exception {
+    // IUserDao dao = getRegistry().getBean(IUserDao.class);
+    // UserGroupBo bo = dao.getUserGroup(id);
+    // if (bo == null) {
+    // return responseRedirect(
+    // samples.controllers.routes.SampleControlPanelController.usergroups(),
+    // VIEW_USERGROUPS, SampleConstants.FLASH_MSG_PREFIX_ERROR
+    // + calcMessages().at("error.usergroup.not_found", id));
+    // } else if (StringUtils.equalsAnyIgnoreCase(id,
+    // SampleConstants.USERGROUP_ID_ADMIN)) {
+    // return responseRedirect(
+    // samples.controllers.routes.SampleControlPanelController.usergroups(),
+    // VIEW_USERGROUPS, SampleConstants.FLASH_MSG_PREFIX_ERROR + calcMessages()
+    // .at("error.usergroup.cannot_delete_sytem_usergroup", id));
+    // }
+    // Html html = render(VIEW_DELETE_USERGROUP,
+    // UserGroupModel.newInstance(bo));
+    // return ok(html);
+    // }
+    //
+    // /**
+    // * Handle POST /<context>/cp/deleteUsergroup?id=<usergroup-id>
+    // */
+    // public Result deleteUsergroupSubmit(String id) throws Exception {
+    // IUserDao dao = getRegistry().getBean(IUserDao.class);
+    // UserGroupBo bo = dao.getUserGroup(id);
+    // if (bo == null) {
+    // return responseRedirect(
+    // samples.controllers.routes.SampleControlPanelController.usergroups(),
+    // VIEW_USERGROUPS, SampleConstants.FLASH_MSG_PREFIX_ERROR
+    // + calcMessages().at("error.usergroup.not_found", id));
+    // } else if (StringUtils.equalsAnyIgnoreCase(id,
+    // SampleConstants.USERGROUP_ID_ADMIN)) {
+    // return responseRedirect(
+    // samples.controllers.routes.SampleControlPanelController.usergroups(),
+    // VIEW_USERGROUPS, SampleConstants.FLASH_MSG_PREFIX_ERROR + calcMessages()
+    // .at("error.usergroup.cannot_delete_sytem_usergroup", id));
+    // }
+    // DaoResult result = dao.delete(bo);
+    // if (result.getStatus() == DaoOperationStatus.SUCCESSFUL) {
+    // return responseRedirect(
+    // samples.controllers.routes.SampleControlPanelController.usergroups(),
+    // VIEW_USERGROUPS, calcMessages().at("msg.delete_usergroup.done",
+    // bo.getId()));
+    // } else {
+    // return responseRedirect(
+    // samples.controllers.routes.SampleControlPanelController.usergroups(),
+    // VIEW_USERGROUPS, SampleConstants.FLASH_MSG_PREFIX_WARNING
+    // + calcMessages().at("msg.delete_usergroup.failed", bo.getId()));
+    // }
+    // }
 }
--- src/main/g8/app/samples/utils/UserUtils.java
+++ src/main/g8/app/samples/utils/UserUtils.java
@@ -4,7 +4,10 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.github.ddth.commons.utils.HashUtils;
 
+import modules.registry.RegistryGlobal;
+import samples.bo.user.IUserDao;
 import samples.bo.user.UserBo;
+import samples.bo.user.UserGroupBo;
 
 /**
  * User-related helper class.
@@ -39,5 +42,43 @@ public class UserUtils {
             return false;
         }
         return StringUtils.equalsAnyIgnoreCase(user.getPassword(), encryptPassword(inputPassword));
+    }
+
+    /**
+     * Get all available users.
+     *
+     * @return
+     */
+    public static UserBo[] allUsers() {
+        return RegistryGlobal.registry.getBean(IUserDao.class).getAllUsers()
+                .toArray(UserBo.EMPTY_ARRAY);
+    }
+
+    /**
+     * Count number of user accounts.
+     *
+     * @return
+     */
+    public static int countUsers() {
+        return RegistryGlobal.registry.getBean(IUserDao.class).getAllUsers().size();
+    }
+
+    /**
+     * Get all available user groups.
+     *
+     * @return
+     */
+    public static UserGroupBo[] allUsergroups() {
+        return RegistryGlobal.registry.getBean(IUserDao.class).getAllUserGroups()
+                .toArray(UserGroupBo.EMPTY_ARRAY);
+    }
+
+    /**
+     * Count number of user groups.
+     *
+     * @return
+     */
+    public static int countUsergroups() {
+        return RegistryGlobal.registry.getBean(IUserDao.class).getAllUserGroups().size();
     }
 }
